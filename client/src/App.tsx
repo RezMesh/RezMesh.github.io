e64decfa86ec1745b98209af5b1179e5aa927761
SMozaff <2.5222175e+08+SMozaff@users.noreply.github.com>
2026-08-22 19:34:04 +0000

    Reduce initial client rendering payload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,15 @@
 /**
  * Signal Field application shell: light-first technical case study with an accessible optional dark inspection mode.
  */
-import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light" switchable>
-        <Router />
+        <Home />
       </ThemeProvider>
     </ErrorBoundary>
   );
